4e8d287d7abcabb8f4d08704893e923611900629
Alex Ghiondea <ghiondea.alexandru@microsoft.com>
2018-07-23 10:47:34 -0700

    Add project for generating a DGML file from the results.

---
+++ src/lib/Microsoft.Fx.Portability.Reports.DGML/DGMLOutputWriter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Fx.Portability.ObjectModel;
+using Microsoft.Fx.Portability.Reporting;
+using System;
+using System.IO;
+
+namespace Microsoft.Fx.Portability.Reports.DGML
+{
+    public class DGMLOutputWriter : IReportWriter
+    {
+        public ResultFormatInformation Format => throw new NotImplementedException();
+
+        public void WriteStream(Stream stream, AnalyzeResponse response)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
