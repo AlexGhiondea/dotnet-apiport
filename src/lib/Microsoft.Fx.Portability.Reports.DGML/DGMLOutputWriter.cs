bfc1a4b927088bd2b4c69aea4621ac3fdb0ea713
Alex Ghiondea <ghiondea.alexandru@microsoft.com>
2018-07-23 11:46:57 -0700

    Make sure that the DGML output writer is hooked up in the system.
    Introducing a breaking change in the API where we have to pass in the request as well (as that contains additional infomation about the user assemblies).

--- src/lib/Microsoft.Fx.Portability.Reports.DGML/DGMLOutputWriter.cs
+++ src/lib/Microsoft.Fx.Portability.Reports.DGML/DGMLOutputWriter.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Fx.Portability.ObjectModel;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Fx.Portability.ObjectModel;
 using Microsoft.Fx.Portability.Reporting;
 using System;
 using System.IO;
@@ -7,9 +10,14 @@ namespace Microsoft.Fx.Portability.Reports.DGML
 {
     public class DGMLOutputWriter : IReportWriter
     {
-        public ResultFormatInformation Format => throw new NotImplementedException();
+        public ResultFormatInformation Format => new ResultFormatInformation()
+        {
+            DisplayName="DGML",
+            MimeType="application/xml",
+            FileExtension=".dgml"
+        };
 
-        public void WriteStream(Stream stream, AnalyzeResponse response)
+        public void WriteStream(Stream stream, AnalyzeResponse response, AnalyzeRequest request)
         {
             throw new NotImplementedException();
         }
