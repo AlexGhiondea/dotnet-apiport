632025056fb8a969f08a66a2be4c50bb27aa6a70
Alex Ghiondea <ghiondea.alexandru@microsoft.com>
2018-07-26 22:56:55 -0700

    Refactor the DGML specific code into a separate class.

--- src/lib/Microsoft.Fx.Portability.Reports.DGML/DGMLOutputWriter.cs
+++ src/lib/Microsoft.Fx.Portability.Reports.DGML/DGMLOutputWriter.cs
@@ -23,17 +23,7 @@ namespace Microsoft.Fx.Portability.Reports.DGML
             FileExtension = ".dgml"
         };
 
-        XDocument file;
-
-        public DGMLOutputWriter()
-        {
-            file = XDocument.Parse(_template);
-            XElement root = file.Root;
-            //TODO: root.SetAttributeValue("Title", request.ApplicationName);
-
-            nodes = root.Element(_nameSpace + "Nodes");
-            links = root.Element(_nameSpace + "Links");
-        }
+        private DGMLManager dgml = new DGMLManager();
 
         public void WriteStream(Stream stream, AnalyzeResponse response, AnalyzeRequest request)
         {
@@ -60,21 +50,24 @@ namespace Microsoft.Fx.Portability.Reports.DGML
 
                     // generate the node
                     string tfm = targets[i].FullName;
-                    GetOrCreateGuid($"{node.Assembly},TFM:{tfm}", out Guid nodeGuid);
+                    dgml.GetOrCreateGuid($"{node.Assembly},TFM:{tfm}", out Guid nodeGuid);
 
-                    AddNode(nodeGuid, $"{node.SimpleName}, {portabilityIndex}%", node.IsMissing ? "Unresolved" : GetCategory(portabilityIndex), portabilityIndex.ToString(), group: missingTypes.Length == 0 ? null : "Collapsed");
+                    dgml.AddNode(nodeGuid, $"{node.SimpleName}, {portabilityIndex}%",
+                        node.IsMissing ? "Unresolved" : GetCategory(portabilityIndex),
+                        portabilityIndex.ToString(),
+                        group: missingTypes.Length == 0 ? null : "Collapsed");
 
-                    if (_nodesDictionary.TryGetValue(tfm, out Guid frameworkGuid))
+                    if (dgml.TryGetId(tfm, out Guid frameworkGuid))
                     {
-                        AddLink(frameworkGuid, nodeGuid, "Contains");
+                        dgml.AddLink(frameworkGuid, nodeGuid, "Contains");
                     }
 
 
                     if (!string.IsNullOrEmpty(missingTypes))
                     {
                         Guid commentGuid = Guid.NewGuid();
-                        AddNode(commentGuid, missingTypes, "Comment");
-                        AddLink(nodeGuid, commentGuid, "Contains");
+                        dgml.AddNode(commentGuid, missingTypes, "Comment");
+                        dgml.AddLink(nodeGuid, commentGuid, "Contains");
                     }
                 }
             }
@@ -86,23 +79,18 @@ namespace Microsoft.Fx.Portability.Reports.DGML
                 {
                     // generate the node
                     string tfm = targets[i].FullName;
-                    GetOrCreateGuid($"{node.Assembly},TFM:{tfm}", out Guid nodeGuid);
+                    dgml.GetOrCreateGuid($"{node.Assembly},TFM:{tfm}", out Guid nodeGuid);
 
                     foreach (var refNode in node.Nodes)
                     {
-                        GetOrCreateGuid($"{refNode.Assembly},TFM:{tfm}", out Guid refNodeGuid);
+                        dgml.GetOrCreateGuid($"{refNode.Assembly},TFM:{tfm}", out Guid refNodeGuid);
 
-                        AddLink(nodeGuid, refNodeGuid);
+                        dgml.AddLink(nodeGuid, refNodeGuid);
                     }
                 }
             }
 
-            using (var ms = new MemoryStream())
-            {
-                file.Save(ms);
-                ms.Position = 0;
-                ms.CopyTo(stream);
-            };
+            dgml.Save(stream);
 
             return;
         }
@@ -123,21 +111,9 @@ namespace Microsoft.Fx.Portability.Reports.DGML
             {
                 string targetFramework = targets[i].FullName;
                 Guid nodeGuid = Guid.NewGuid();
-                _nodesDictionary.Add(targetFramework, nodeGuid);
-                AddNode(nodeGuid, targetFramework, "Target", group: "Expanded");
+                dgml.AddId(targetFramework, nodeGuid);
+                dgml.AddNode(nodeGuid, targetFramework, "Target", group: "Expanded");
             }
-        }
-
-        private bool GetOrCreateGuid(string nodeLabel, out Guid guid)
-        {
-            if (!_nodesDictionary.TryGetValue(nodeLabel, out guid))
-            {
-                guid = Guid.NewGuid();
-                _nodesDictionary.Add(nodeLabel, guid);
-                return false;
-            }
-
-            return true;
         }
 
         private static string GetCategory(double probabilityIndex)
@@ -154,72 +130,9 @@ namespace Microsoft.Fx.Portability.Reports.DGML
             return "Low";
         }
 
-        private void AddLink(Guid source, Guid target, string category = null)
-        {
-            var element = new XElement(_nameSpace + "Link",
-                new XAttribute("Source", source),
-                new XAttribute("Target", target));
 
-            if (category != null)
-                element.SetAttributeValue("Category", category);
 
-            links.Add(element);
-        }
 
-        private void AddNode(Guid id, string label, string category, string portabilityIndex = null, string group = null)
-        {
-            var element = new XElement(_nameSpace + "Node",
-                new XAttribute("Id", id),
-                new XAttribute("Label", label),
-                new XAttribute("Category", category));
-
-            if (portabilityIndex != null)
-                element.SetAttributeValue("PortabilityIndex", portabilityIndex);
-            if (group != null)
-                element.SetAttributeValue("Group", group);
-
-            nodes.Add(element);
-        }
-
-        private XElement nodes;
-
-        private XElement links;
-
-        private readonly Dictionary<string, Guid> _nodesDictionary = new Dictionary<string, Guid>();
-
-        private readonly XNamespace _nameSpace = "http://schemas.microsoft.com/vs/2009/dgml";
-
-        private readonly string _template =
-            @"<?xml version=""1.0"" encoding=""utf-8""?>
-            <DirectedGraph xmlns=""http://schemas.microsoft.com/vs/2009/dgml"" Background=""grey"">
-            <Nodes>
-            </Nodes>
-            <Links>
-            </Links>
-            <Categories>
-                <Category Id=""VeryHigh"" Background=""#009933"" />
-                <Category Id=""High"" Background=""#ffff66"" />
-                <Category Id=""Medium"" Background=""#ff9900"" />
-                <Category Id=""MediumLow"" Background=""#ff3300"" />
-                <Category Id=""Low"" Background=""#990000"" />
-                <Category Id=""Target"" Background=""white"" />
-                <Category Id=""Unresolved"" Background=""red"" />
-                <Category Id=""Comment"" Label=""Comment"" Description=""Represents a user defined comment on the diagram"" NavigationActionLabel=""Comments"" />
-            </Categories>
-            <Properties>
-                <Property Id=""PortabilityIndex"" Label=""Portability Index"" DataType=""System.String"" />
-            </Properties>
-            <Styles>
-                <Style TargetType=""Node"" GroupLabel=""Comment"" ValueLabel=""Has comment"">
-                  <Condition Expression = ""HasCategory('Comment')"" />
-                  <Setter Property=""Background"" Value=""#FFFFFACD"" />
-                  <Setter Property=""Stroke"" Value=""#FFE5C365"" />
-                  <Setter Property=""StrokeThickness"" Value=""1"" />
-                  <Setter Property=""NodeRadius"" Value=""2"" />
-                  <Setter Property=""MaxWidth"" Value=""250"" />
-                </Style>
-              </Styles>W
-            </DirectedGraph>";
 
     }
 }
