06327506086073a53a67bfb73199f9b4736d2cb6
Alex Ghiondea <ghiondea.alexandru@microsoft.com>
2018-07-26 22:42:06 -0700

    Move each type into its own class.

--- src/lib/Microsoft.Fx.Portability.Reports.DGML/ReferenceGraph.cs
+++ src/lib/Microsoft.Fx.Portability.Reports.DGML/ReferenceGraph.cs
@@ -1,10 +1,9 @@
-﻿using Microsoft.Fx.Portability.ObjectModel;
-using Microsoft.Fx.Portability.Reporting.ObjectModel;
-using System;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Fx.Portability.ObjectModel;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
-using System.Text;
 
 namespace Microsoft.Fx.Portability.Reports.DGML
 {
@@ -88,61 +87,5 @@ namespace Microsoft.Fx.Portability.Reports.DGML
             Nodes.Add(node, node);
             return node;
         }
-    }
-
-    class ReferenceNodeComparer : IEqualityComparer<ReferenceNode>
-    {
-        public bool Equals(ReferenceNode x, ReferenceNode y)
-        {
-            return x.Assembly == y.Assembly;
-        }
-
-        public int GetHashCode(ReferenceNode obj)
-        {
-            return obj.Assembly.GetHashCode();
-        }
-    }
-
-    class ReferenceNode
-    {
-        public string SimpleName
-        {
-            get
-            {
-                if (!IsMissing)
-                    return new AssemblyName(Assembly).Name;
-
-                return "Unresolved: " + new AssemblyName(Assembly).Name;
-            }
-        }
-        public ReferenceNode(string AssemblyName, bool unresolved = false)
-        {
-            Assembly = AssemblyName;
-            this.Unresolved = unresolved;
-            Nodes = new HashSet<ReferenceNode>();
-        }
-        public override int GetHashCode()
-        {
-            return Assembly.GetHashCode();
-        }
-
-        public void AddReferenceToNode(ReferenceNode node)
-        {
-            Nodes.Add(node);
-        }
-
-        public override string ToString()
-        {
-            return Assembly;
-        }
-
-        public List<TargetUsageInfo> UsageData { get; set; }
-
-        public string Assembly { get; set; }
-
-        public bool Unresolved { get; set; }
-
-        public HashSet<ReferenceNode> Nodes { get; set; }
-        public bool IsMissing { get; internal set; }
     }
 }
